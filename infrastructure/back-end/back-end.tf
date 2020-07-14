30d2bc9ba146e424aad5c52e1cb56d36da5f0c41
dboulet01 <dboulet01@gmail.com>
2020-07-13 22:54:45 -0400

    commit

--- infrastructure/back-end/back-end.tf
+++ infrastructure/back-end/back-end.tf
@@ -1,4 +1,31 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
+resource "azurerm_resource_group" "back_end" {
+  name     = "back-end"
+  location = "eastus2"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "functionsapptestsa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "azure-functions-test-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                      = "test-azure-functions"
+  location                  = azurerm_resource_group.example.location
+  resource_group_name       = azurerm_resource_group.example.name
+  app_service_plan_id       = azurerm_app_service_plan.example.id
+  storage_connection_string = azurerm_storage_account.example.primary_connection_string
 }
---
+++ infrastructure/front-end/front-end.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "front_end" {
+  name     = "front-end"
+  location = "eastus2"
+}
+
+resource "azurerm_storage_account" "front_end" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.front_end.name
+  location                 = azurerm_resource_group.front_end.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  static_website {
+    index_document     = "index.vue"
+    error_404_document = "error.vue"
+  }
+
+}
+
+resource "azurerm_storage_container" "website" {
+  name                  = "$web"
+  storage_account_name  = azurerm_storage_account.front_end.name
+  container_access_type = "public"
+}
+
