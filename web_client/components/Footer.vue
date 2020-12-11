e01bef2e7dcf0965a3f70262f7e7f0dc63d752a7
Justin Chi <jchi@memsql.com>
2020-12-11 11:47:26 -0800

    Build out FE footer (#221)
    * Build out FE footer
    
    * refactor

--- web_client/components/Footer.vue
+++ web_client/components/Footer.vue
@@ -1,90 +1,62 @@
 <template>
-    <footer class="usa-footer usa-footer--slim">
-        <div class="grid-container usa-footer__return-to-top">
-            <a href="javascript:void(0);" @click="scrollToTop">Return to top</a>
-        </div>
-        <div class="usa-footer__primary-section">
-            <div class="usa-footer__primary-container grid-row">
-                <div class="mobile-lg:grid-col-8">
-                    <nav class="usa-footer__nav" aria-label="Footer navigation">
-                        <ul class="grid-row grid-gap">
-                            <li
-                                class="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content"
-                            >
-                                <a
-                                    class="usa-footer__primary-link"
-                                    href="javascript:void(0);"
-                                    >Documentation</a
-                                >
-                            </li>
-                            <li
-                                class="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content"
-                            >
-                                <a
-                                    class="usa-footer__primary-link"
-                                    href="javascript:void(0);"
-                                    >Features</a
-                                >
-                            </li>
-                            <li
-                                class="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content"
-                            >
-                                <a
-                                    class="usa-footer__primary-link"
-                                    href="javascript:void(0);"
-                                    >About us</a
-                                >
-                            </li>
-                            <li
-                                class="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content"
-                            >
-                                <a
-                                    class="usa-footer__primary-link"
-                                    href="javascript:void(0);"
-                                    >Getting started</a
-                                >
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="mobile-lg:grid-col-4">
-                    <address class="usa-footer__address">
-                        <div class="grid-row grid-gap">
-                            <div
-                                class="grid-col-auto mobile-lg:grid-col-12 desktop:grid-col-auto"
-                            >
-                                <div class="usa-footer__contact-info">
-                                    <a href="tel:1-800-555-5555"
-                                        >(800) CALL-GOVT</a
-                                    >
-                                </div>
-                            </div>
-                            <div
-                                class="grid-col-auto mobile-lg:grid-col-12 desktop:grid-col-auto"
-                            >
-                                <div class="usa-footer__contact-info">
-                                    <a href="mailto:info@agency.gov"
-                                        >info@agency.gov</a
-                                    >
-                                </div>
-                            </div>
-                        </div>
-                    </address>
-                </div>
+    <footer class="text-white">
+        <div class="return-top">
+            <div class="m-auto py-1 px-2 md:px-4 xl:container">
+                <a class="underline" href="#">Return to top</a>
             </div>
         </div>
-        <div class="usa-footer__secondary-section">
-            <div class="grid-container">
-                <div class="usa-footer__logo grid-row grid-gap-2">
-                    <div class="grid-col-auto">
-                        <img
-                            class="tw-w-full usa-footer__logo-img"
-                            src="~/assets/images/DEPT_OF_TECH_LOGO_WIP.svg"
-                            alt=""
-                        />
+        <div class="head">
+            <div class="head-inner m-auto xl:container md:px-2">
+                <a href="#" class="link">Documentation</a>
+                <a href="#" class="link">Features</a>
+                <a href="#" class="link">Getting Started</a>
+                <a href="#" class="link">About Us</a>
+                <a href="#" class="link">Privacy Policy</a>
+            </div>
+        </div>
+        <div class="bottom pb-2">
+            <div
+                class="m-auto flex flex-col-reverse xl:container md:justify-between md:flex-row md:py-3 md:px-4"
+            >
+                <div class="dept-of-tech flex items-center py-3 px-2 md:pl-0">
+                    <img
+                        class="tech-logo mr-2"
+                        src="@/assets/images/dept-of-tech-logo.svg"
+                        imgAlt="Department of Technology logo"
+                    />
+                    <div class="tech-header flex items-center font-bold">
+                        Department of Technology
                     </div>
-                    <div class="grid-col-auto">
-                        <h3 class="usa-footer__logo-heading">Access America</h3>
+                </div>
+                <div>
+                    <div class="social hidden justify-end md:flex">
+                        <a href="#" class="social-link">
+                            <img src="@/assets/images/social/facebook.svg" />
+                        </a>
+                        <a href="#" class="social-link">
+                            <img src="@/assets/images/social/twitter.svg" />
+                        </a>
+                        <a href="#" class="social-link">
+                            <img src="@/assets/images/social/youtube.svg" />
+                        </a>
+                        <a href="#" class="social-link">
+                            <img src="@/assets/images/social/rss.svg" />
+                        </a>
+                    </div>
+                    <div class="text-white p-2 md:pr-0">
+                        <div
+                            class="contact-header font-bold hidden md:block md:text-right md:mb-1"
+                        >
+                            Agency Contact Center
+                        </div>
+                        <div class="flex justify-between">
+                            <a
+                                class="text-white md:pr-4"
+                                href="tel:+1800-123-4567"
+                                >(800) CALL-GOVT</a
+                            >
+                            <a href="mailto:info@agency.gov">info@agency.gov</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -104,4 +76,76 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.return-top {
+    background: #eeeeee;
+}
+
+.return-top a {
+    color: #0f4f93;
+}
+
+.head {
+    background: #15498d;
+}
+
+.head-inner {
+    @screen md {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    }
+}
+
+.head-inner .link {
+    border-top: 1px solid #112f4e;
+    color: white;
+    padding: 16px;
+    display: block;
+
+    @screen md {
+        border: none;
+    }
+}
+
+.head-inner .link:hover {
+    background: #1855a5;
+}
+
+.bottom {
+    background: #112f4e;
+}
+
+.tech-logo {
+    width: 48px;
+    height: 48px;
+
+    @screen md {
+        width: 80px;
+        height: 80px;
+    }
+}
+
+.tech-header {
+    font-size: 24px;
+    line-height: 26px;
+}
+
+.social-link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 48px;
+    height: 48px;
+    background: #f2f2f2;
+    margin-left: 8px;
+}
+
+.social-link:hover {
+    opacity: 0.75;
+}
+
+.contact-header {
+    font-size: 22px;
+    line-height: 26px;
+}
+</style>
--- web_client/layouts/default.vue
+++ web_client/layouts/default.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
         <nuxt />
+        <Footer />
     </div>
 </template>
+<script>
+import Footer from '@/components/Footer'
+
+export default {
+    name: 'Layout',
+    components: { Footer },
+}
+</script>
 
 <style></style>
