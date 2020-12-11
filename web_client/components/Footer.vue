1fdae5b65321aa47fe3649977820c649b4f10aa7
Justin Chi <jchi@memsql.com>
2020-12-11 01:40:26 -0800

    refactor

--- web_client/components/Footer.vue
+++ web_client/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-    <footer class="footer">
+    <footer class="text-white">
         <div class="return-top">
-            <div class="return-top-inner m-auto">
-                <a href="#">Return to top</a>
+            <div class="m-auto py-1 px-2 md:px-4 xl:container">
+                <a class="underline" href="#">Return to top</a>
             </div>
         </div>
         <div class="head">
-            <div class="head-inner m-auto">
+            <div class="head-inner m-auto xl:container md:px-2">
                 <a href="#" class="link">Documentation</a>
                 <a href="#" class="link">Features</a>
                 <a href="#" class="link">Getting Started</a>
@@ -14,35 +14,47 @@
                 <a href="#" class="link">Privacy Policy</a>
             </div>
         </div>
-        <div class="bottom">
-            <div class="bottom-inner m-auto">
-                <div class="dept-of-tech">
+        <div class="bottom pb-2">
+            <div
+                class="m-auto flex flex-col-reverse xl:container md:justify-between md:flex-row md:py-3 md:px-4"
+            >
+                <div class="dept-of-tech flex items-center py-3 px-2 md:pl-0">
                     <img
-                        class="logo"
+                        class="tech-logo mr-2"
                         src="@/assets/images/dept-of-tech-logo.svg"
                         imgAlt="Department of Technology logo"
                     />
-                    <div class="header">Department of Technology</div>
+                    <div class="tech-header flex items-center font-bold">
+                        Department of Technology
+                    </div>
                 </div>
-                <div class="info-section">
-                    <div class="social">
-                        <a href="#" class="link">
+                <div>
+                    <div class="social hidden justify-end md:flex">
+                        <a href="#" class="social-link">
                             <img src="@/assets/images/social/facebook.svg" />
                         </a>
-                        <a href="#" class="link">
+                        <a href="#" class="social-link">
                             <img src="@/assets/images/social/twitter.svg" />
                         </a>
-                        <a href="#" class="link">
+                        <a href="#" class="social-link">
                             <img src="@/assets/images/social/youtube.svg" />
                         </a>
-                        <a href="#" class="link">
+                        <a href="#" class="social-link">
                             <img src="@/assets/images/social/rss.svg" />
                         </a>
                     </div>
-                    <div class="contact-wrapper">
-                        <div class="header">Agency Contact Center</div>
-                        <div class="info">
-                            <a href="tel:+1800-123-4567">(800) CALL-GOVT</a>
+                    <div class="text-white p-2 md:pr-0">
+                        <div
+                            class="contact-header font-bold hidden md:block md:text-right md:mb-1"
+                        >
+                            Agency Contact Center
+                        </div>
+                        <div class="flex justify-between">
+                            <a
+                                class="text-white md:pr-4"
+                                href="tel:+1800-123-4567"
+                                >(800) CALL-GOVT</a
+                            >
                             <a href="mailto:info@agency.gov">info@agency.gov</a>
                         </div>
                     </div>
@@ -64,169 +76,76 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
-.footer {
-    $max-screen-width: 1280px;
+<style scoped>
+.return-top {
+    background: #eeeeee;
+}
 
+.return-top a {
+    color: #0f4f93;
+}
+
+.head {
+    background: #15498d;
+}
+
+.head-inner {
+    @screen md {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    }
+}
+
+.head-inner .link {
+    border-top: 1px solid #112f4e;
     color: white;
+    padding: 16px;
+    display: block;
 
-    .return-top {
-        background: #eeeeee;
-
-        .return-top-inner {
-            padding: 4px 16px;
-            max-width: $max-screen-width;
-
-            @screen md {
-                padding: 4px 32px;
-            }
-
-            a {
-                color: #0f4f93;
-                text-decoration: underline;
-            }
-        }
+    @screen md {
+        border: none;
     }
+}
 
-    .head {
-        background: #15498d;
+.head-inner .link:hover {
+    background: #1855a5;
+}
 
-        .head-inner {
-            max-width: $max-screen-width;
+.bottom {
+    background: #112f4e;
+}
 
-            @screen md {
-                display: grid;
-                grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+.tech-logo {
+    width: 48px;
+    height: 48px;
 
-                padding: 0 16px;
-            }
-
-            .link {
-                color: white;
-                white-space: nowrap;
-                border-top: 1px solid #112f4e;
-                padding: 16px;
-                line-height: 19px;
-                display: block;
-
-                @screen md {
-                    border: none;
-                }
-
-                &:hover {
-                    background: #1855a5;
-                }
-            }
-        }
+    @screen md {
+        width: 80px;
+        height: 80px;
     }
+}
 
-    .bottom {
-        background: #112f4e;
-        padding-bottom: 16px;
+.tech-header {
+    font-size: 24px;
+    line-height: 26px;
+}
 
-        .bottom-inner {
-            display: flex;
-            flex-direction: column-reverse;
-            max-width: $max-screen-width;
+.social-link {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 48px;
+    height: 48px;
+    background: #f2f2f2;
+    margin-left: 8px;
+}
 
-            @screen md {
-                justify-content: space-between;
-                flex-direction: row;
-                padding: 24px 32px;
-            }
-        }
-    }
+.social-link:hover {
+    opacity: 0.75;
+}
 
-    .dept-of-tech {
-        display: flex;
-        padding: 24px 16px;
-
-        @screen md {
-            padding-left: 0;
-        }
-
-        .logo {
-            width: 48px;
-            height: 48px;
-            margin-right: 16px;
-
-            @screen md {
-                width: 80px;
-                height: 80px;
-            }
-        }
-
-        .header {
-            font-size: 24px;
-            line-height: 26px;
-            font-weight: 700;
-            display: flex;
-            align-items: center;
-        }
-    }
-
-    .section-info {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end;
-    }
-
-    .contact-wrapper {
-        color: white;
-        padding: 16px;
-
-        @screen md {
-            padding-right: 0;
-        }
-
-        .header {
-            display: none;
-            font-size: 22px;
-            font-weight: 700;
-            line-height: 26px;
-
-            @screen md {
-                display: block;
-                text-align: right;
-                margin-bottom: 8px;
-            }
-        }
-        .info {
-            display: flex;
-            justify-content: space-between;
-
-            a {
-                color: white;
-                text-decoration: none;
-
-                &:first-child {
-                    padding-right: 32px;
-                }
-            }
-        }
-    }
-
-    .social {
-        display: none;
-        justify-content: flex-end;
-
-        @screen md {
-            display: flex;
-        }
-
-        .link {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 48px;
-            height: 48px;
-            background: #f2f2f2;
-            margin-left: 8px;
-
-            &:hover {
-                opacity: 0.75;
-            }
-        }
-    }
+.contact-header {
+    font-size: 22px;
+    line-height: 26px;
 }
 </style>
