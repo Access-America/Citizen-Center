f97bb88269947ddd1fbbca1a600416a8fb32b916
Ricky <ricky.a.lalwani@gmail.com>
2021-07-29 21:37:17 -0700

    Update ESLint to latest

--- web_client/.eslintrc.js
+++ web_client/.eslintrc.js
@@ -1,20 +1,21 @@
 module.exports = {
     root: true,
     env: {
-        browser: true,
-        node: true,
+      browser: true,
+      node: true
     },
     parserOptions: {
-        parser: 'babel-eslint',
+      parser: '@babel/eslint-parser',
+      requireConfigFile: false
     },
     extends: [
-        '@nuxtjs',
-        'prettier',
-        'prettier/vue',
-        'plugin:prettier/recommended',
-        'plugin:nuxt/recommended',
+      '@nuxtjs',
+      'plugin:nuxt/recommended',
+      'prettier'
     ],
-    plugins: ['prettier'],
+    plugins: [
+    ],
     // add your custom rules here
-    rules: {},
-}
+    rules: {}
+  }
+  
